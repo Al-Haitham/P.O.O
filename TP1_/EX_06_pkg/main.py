579b982c405851ed9ada1f664a7f081a1a7daa6a
Al-Haitam <study.haitham@gmail.com>
2026-03-30 11:01:38 +0100

    CalcPOOapp final

--- TP1_/EX_06_pkg/main.py
+++ TP1_/EX_06_pkg/main.py
@@ -1,8 +1,5 @@
-from src.MyClasses import Cercle
-import math
+from src.MyClasses import Calcul
 
 #Instanciation
-c1=Cercle(7,3,math.sqrt(8))
-print(c1.surface())
-print(c1.perimetre())
-print(c1.testAppartenance(5,2))
+cal1=Calcul()
+print(cal1.listDiv(10))
--- TP1_/EX_06_pkg/src/MyClasses.py
+++ TP1_/EX_06_pkg/src/MyClasses.py
@@ -3,4 +3,47 @@ class Calcul:
     def __init__(self):
         pass
 
-    
+    def Factorielle(self,n):
+        f=1
+        if n==0:
+            print(f"{n}!= ",end="")
+            return 1
+        else:
+            for i in range(1,n+1):
+                f*=i
+            print(f"{n}!= ",end="")
+            return f
+
+    def Somme(self,n):
+        s=0
+        for i in range(1,n+1):
+            s+=i
+        return s
+        
+    def tableMult(self,n):
+        if n<0:
+            print(f"{n} est negative")
+        else:
+            for i in range(1,11):
+                print(f"{i} x {n} = {i*n}")
+
+    def allTablesMult(self):
+        for i in range(1,11):
+            print(f"-> table de {i}:")
+            for j in range(1,11):
+                print(f"{i} x {j} = {i*j}")
+            print("")
+
+    def listDiv(self,n):
+        Ldiv=[]
+        for i in range(1,n+1):
+            if n%i==0:
+                Ldiv.append(i)
+        return Ldiv
+
+    def listDivPrim(self,n):
+        LdivPr=[]
+        for d in self.listDiv(n):
+            if len(self.listDiv(d))==2:
+                LdivPr.append(d)
+        return LdivPr
