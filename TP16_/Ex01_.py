936f70a1bce085de499ca441a60a863b7f11c141
鹰Al-Haitam <study.haitham@gmail.com>
2026-07-03 10:11:40 +0100

    new

--- TP16_/Ex01_.py
+++ TP16_/Ex01_.py
@@ -6,33 +6,56 @@ cal.title("Calculatrice")
 
 res=tk.StringVar()
 
-tk.Label(cal,text="nombre 1: ").pack()
-n1=tk.Entry(cal).pack(pady=5)
+#tk.Label(cal,text="nombre 1: ").pack()
+n1=tk.Entry(cal)
+n1.grid(row=0,column=0)
 
-tk.Label(cal,text="nombre 2: ").pack()
-n2=tk.Entry(cal).pack(pady=5)
+#tk.Label(cal,text="nombre 2: ").pack()
+n2=tk.Entry(cal)
+n2.grid(row=0,column=1)
 
 def calculer(op):
-     if not n1.get() or not n2.get():
-            res.set("Erreur : Champs vides")
-            return
+    if not n1.get() or not n2.get():
+        res.set("Error: Champs vides!!")
+        return
+    
+    val1=float(n1.get())
+    val2=float(n2.get())
+    result=0
+    if op=="add":
+        result=val1+val2
+    elif op=="sub":
+        result=val1-val2
+    elif op=="mul":
+        result=val1*val2
+    elif op=="div":
+        if val2==0:
+            raise ZeroDivisionError
+        result=val1/val2
+    elif op=="mod":
+        result=val1%val2
+    elif op=="pui":
+        result=val1**val2
+    res.set(result)
+    
+btn_frame=tk.Frame(cal)
+btn_frame.grid(row=1,column=0)
 
-        # Convert to float
-        val1 = float(n1.get())
-        val2 = float(n2.get())
-        
-        res = 0
-        if op == "add":
-            res = val1 + val2
-        elif op == "sub":
-            res = val1 - val2
-        elif op == "mul":
-            res = val1 * val2
-        elif op == "div":
-            if val2 == 0:
-                raise ZeroDivisionError
-            res = val1 / val2
-            
-        res.set(f"Résultat : {res}")
+sub_btnF_1=tk.Frame(btn_frame)
+sub_btnF_1.grid(row=0,column=0)
+
+sub_btnF_2=tk.Frame(btn_frame)
+sub_btnF_2.grid(row=0,column=1)
+
+tk.Button(sub_btnF_1, text="+", command=lambda: calculer("add")).grid(row=0, column=0)
+tk.Button(sub_btnF_1, text="-", command=lambda: calculer("sub")).grid(row=0,column=1)
+tk.Button(sub_btnF_1, text="x", command=lambda: calculer("mul")).grid(row=1, column=0)
+tk.Button(sub_btnF_1, text="/", command=lambda: calculer("div")).grid(row=1,column=1)
+tk.Button(sub_btnF_2, text="^", command=lambda: calculer("pui")).grid(row=0, column=0)
+tk.Button(sub_btnF_2, text="%", command=lambda: calculer("mod")).grid(row=1,column=0)
+
+
+resZone=tk.Label(cal, textvariable=res)
+resZone.grid(row=1,column=1)
 
 cal.mainloop()
